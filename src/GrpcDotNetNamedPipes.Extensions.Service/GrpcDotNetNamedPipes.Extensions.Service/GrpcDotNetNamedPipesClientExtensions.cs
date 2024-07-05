9300394939f3f6a50c13eb90a4ad7665fb3f3b3d
芬里尔吉崽 <nevermore.wd@me.com>
2024-07-05 18:32:24 +0800

    init commit

---
+++ src/GrpcDotNetNamedPipes.Extensions.Service/GrpcDotNetNamedPipes.Extensions.Service/GrpcDotNetNamedPipesClientExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GrpcDotNetNamedPipes.Extensions.Service
+{
+    public static class GrpcDotNetNamedPipesClientExtensions
+    {
+        public static void AddNamedPipeChannelOptions(this IServiceCollection serviceCollection, NamedPipeChannelOptions namedPipeChannelOptions)
+        {
+            serviceCollection.AddSingleton(namedPipeChannelOptions);
+        }
+        public static void AddNamedPipeChannelOptions(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<NamedPipeChannelOptions>();
+        }
+        public static void AddNamedPipeChannel(this IServiceCollection serviceCollection, string serverName, string pipeName)
+        {
+            serviceCollection.AddSingleton(sp =>
+            {
+                var options = sp.GetRequiredService<NamedPipeChannelOptions>();
+                if (options != null)
+                {
+                    return new NamedPipeChannel(serverName, pipeName, options);
+                }
+                else
+                {
+
+                    return new NamedPipeChannel(serverName, pipeName);
+                }
+            });
+        }
+        public static void AddGrpcNamedPipesClient<TClient>(this IServiceCollection serviceCollection) where TClient : ClientBase<TClient>
+        {
+            serviceCollection.AddSingleton(sp =>
+            {
+                var options = sp.GetRequiredService<NamedPipeChannel>();
+                if (options == null)
+                {
+                    throw new TypeLoadException($"{typeof(TClient)} requires param:{typeof(NamedPipeChannel)},Please ensure it has been registered!");
+                }
+                var clientInstance = ActivatorUtilities.CreateInstance<TClient>(sp);
+                return clientInstance;
+            });
+        }
+    }
+}
---
+++ src/GrpcDotNetNamedPipes.Extensions.Service/GrpcDotNetNamedPipes.Extensions.Service/GrpcDotNetNamedPipesServerExtensions.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net.Sockets;
+using System.Reflection;
+
+namespace GrpcDotNetNamedPipes.Extensions.Service
+{
+    public static class GrpcDotNetNamedPipesServerExtensions
+    {
+        public static void AddGrpcNamedPipesService<TService>(this IServiceCollection serviceCollection) where TService : class
+        {
+            serviceCollection.AddSingleton(sp =>
+            {
+                var service = ActivatorUtilities.CreateInstance<TService>(sp)!;
+                var server = sp.GetRequiredService<NamedPipeServer>();
+
+                if (server == null)
+                {
+                    throw new TypeLoadException($"Please ensure the {typeof(NamedPipeServer)} has been registered!");
+                }
+
+                var bindServiceMethod = ResolveBindServiceMethod<TService>();
+                bindServiceMethod.Invoke(null, [server.ServiceBinder, service]);
+                return service;
+            });
+        }
+
+        public static void AddGrpcNamedPipesServer(this IServiceCollection serviceCollection, string pipeName)
+        {
+            if (string.IsNullOrWhiteSpace(pipeName))
+            {
+                throw new ArgumentNullException(nameof(pipeName));
+            }
+
+            serviceCollection.AddKeyedSingleton(pipeName, 
+                (sp, key) => 
+                {
+                    if (sp.GetRequiredKeyedService<NamedPipeServer>(key) != null)
+                    {
+                        throw new InvalidOperationException($"You have registered an instance of {typeof(NamedPipeServer)} with pipename:{key}");
+                    }
+                    return new NamedPipeServer(key!.ToString()); 
+                });
+        }
+        /// <summary>
+        /// Affects AOT compilation
+        /// todo...
+        /// </summary>
+        /// <typeparam name="TService"></typeparam>
+        /// <returns></returns>
+        /// <exception cref="TypeLoadException"></exception>
+
+        private static MethodInfo ResolveBindServiceMethod<TService>()
+        {
+            var serviceType = typeof(TService);
+            var baseServiceType = serviceType.BaseType;
+
+            if (baseServiceType == null)
+            {
+                throw new TypeLoadException($"{serviceType} is not a valid GrpcService type! It must have a base class.");
+            }
+
+            if (baseServiceType.IsNested)
+            {
+                Type declaringType = baseServiceType.DeclaringType!;
+
+                if (declaringType.IsAbstract && declaringType.IsSealed && declaringType.IsClass)
+                {
+                    Console.WriteLine("Enclosing Static Class: " + declaringType);
+                }
+
+                MethodInfo? bindServiceMethod = declaringType.GetMethod("BindService", BindingFlags.Static | BindingFlags.Public);
+
+                if (bindServiceMethod == null)
+                {
+                    throw new TypeLoadException($"{serviceType} is not a valid GrpcService type! There's no a static method named of 'BindService' found in {declaringType}");
+                }
+
+                return bindServiceMethod;
+            }
+            else
+            {
+                throw new TypeLoadException($"{serviceType} is not a valid GrpcService type! It's base class must be a nested class.");
+            }
+        }
+    }
+}
